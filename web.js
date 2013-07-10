f6659127a07c30c6d3c05c8c5606c15ca4ef1809
Fanny Ho <fanny543.ho@gmail.com>
2013-07-10 02:46:40 +0000

    read Hello World from index.html

--- web.js
+++ web.js
@@ -2,8 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var info = fs.readFileSync('index.html').toString();
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(info);
 });
 
 var port = process.env.PORT || 5000;
